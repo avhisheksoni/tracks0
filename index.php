aa86bcd2d49d4b1ea6f175e25ac9427fe6a544c5
avhisheksoni <avhisheksoni54@gmail.com>
2020-02-07 18:52:20 +0530

    practer

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
 
 echo "tracks0 git hub";
+$kk=  "git config --global user.email avhisheksoni54@gmail.com"
+  $pp = "git config --global user.name avhisheksoni"
+  "git hub"
+  $apnagps_pass = "Abhishek@999" 
+  $apnauser ="abhisheksoni99"
 ?>
